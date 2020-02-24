5b340c5999ed3f8c077918989eae6a92ef1da0fb
NkrCode <61393520+NkrCode@users.noreply.github.com>
2020-02-24 23:59:59 +0200

    Add files via upload

---
+++ MM_Logo.js
@@ -0,0 +1,36 @@
+let logo;
+
+console.log(N.length)
+function setup() {
+    createCanvas(600, 400);
+    logo = new Logo ();
+}
+
+function draw() {
+    background(0);
+    logo.show();
+}
+class Logo{
+
+    constructor() {
+        let N = "*";
+        let M = 5;
+         ;
+
+        for(i=2; i<n; i++ ) {
+        for(N=3;  N<10000;) {
+            console.log(print);
+        
+        }
+        
+        }
+    } 
+
+
+
+
+    }
+ console.log();
+
+
+
